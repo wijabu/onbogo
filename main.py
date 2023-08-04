9402ee3890880452eb746e4b695bd1073faaa886
Wil Buchanan <130495419+wijabu@users.noreply.github.com>
2023-08-04 12:49:02 -0400

    Update main.py
    Added logging to troubleshoot BackgroundScheduler

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import logging
 import atexit
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from onbogo import onbogo, create_app, cfg
+
+logging.basicConfig()
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
 scheduler = BackgroundScheduler(job_defaults={'max_instances': 2}, daemon=True)
 app = create_app()
@@ -10,7 +14,7 @@ app = create_app()
 if __name__ == "__main__":
     # activate scheduler
     scheduler.start()
-    scheduler.add_job(func=onbogo.run_schedule, trigger='cron', day_of_week='fri', hour=12, minute=40)
+    scheduler.add_job(func=onbogo.run_schedule, trigger='cron', day_of_week='fri', hour=12, minute=55)
     atexit.register(lambda: scheduler.shutdown())
 
     # Starts the app
